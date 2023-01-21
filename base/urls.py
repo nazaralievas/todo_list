4fd54439850ede11d749f676da93cfffa3ed332a
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-21 21:54:01 +0600

    Task model and TaskList cbv added

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    path('', tasklist, name='tasklist'),
+    path('', TaskList.as_view(), name='tasklist'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from .models import Task
 
 
-def tasklist(request):
-    return HttpResponse('tasklist')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
