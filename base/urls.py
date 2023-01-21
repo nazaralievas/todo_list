04cbbb4a25931c2bbe4d97fd01a77eee9b8ca6c9
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-21 22:45:28 +0600

    task CreateView added

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', TaskList.as_view(), name='tasklist'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
