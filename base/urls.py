f58920e777fa5b489e865e03f20361aa7b216d62
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-22 02:20:18 +0600

    logging in added

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='task-list'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),    path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -4,7 +4,18 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('task-list')
 
 
 class TaskList(ListView):
