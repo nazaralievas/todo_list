986ff58930bae9be4a2decf2067b3cb5b2167f09
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-22 02:27:16 +0600

    logging out added

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 
 from .models import Task
 
