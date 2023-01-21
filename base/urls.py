b847daf9f720914d4d515633f61c8f59e6ffaf97
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-21 23:05:10 +0600

    TaskUpdate function handled

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasklist'),
+    path('', TaskList.as_view(), name='task-list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
 ]
