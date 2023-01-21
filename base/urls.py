653fc7dab955193b765e8797ca702eae01a6dc1e
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-21 23:17:09 +0600

    task deletion added

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
